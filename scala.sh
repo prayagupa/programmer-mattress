740fba31ccc9b9d223ba0ea5252c8784376f798c
prayagupd <prayag.upd@gmail.com>
2014-12-22 12:00:21 +0545

    FIX scala provision script

--- scala.sh
+++ scala.sh
@@ -3,11 +3,12 @@ installScala(){
  DEFAULT_SOURCE_ROOT_JVM="$HOME"
  SCALA_VERSION="2.10.3"
  WGET_URL="http://www.scala-lang.org/files/archive/scala-$SCALA_VERSION.tgz"
- SCALA_LOCATION_SOURCE="$DEFAULT_SOURCE_ROOT_JVM/scala-$SCALA_VERSION.tgz"
+ SCALA_LOCATION_SOURCE="${DEFAULT_SOURCE_ROOT_JVM}/scala-$SCALA_VERSION.tgz"
+ DEFAULT_INSTALLATION_DEST="/usr/local/"
  wget -O $SCALA_LOCATION_SOURCE --no-clobber $WGET_URL
 
- sudo tar -zxvf $SCALA_LOCATION_SOURCE -C $DEFAULT_INSTALLATION_DEST
- sudo chmod 777 -R $DEFAULT_INSTALLATION_DEST/scala-$SCALA_VERSION
+ sudo tar -zxvf ${SCALA_LOCATION_SOURCE} -C ${DEFAULT_INSTALLATION_DEST}
+ sudo chmod 777 -R ${DEFAULT_INSTALLATION_DEST}/scala-$SCALA_VERSION
 
  cat >> ~/.bash_profile <<'EOF'
   ###############################
